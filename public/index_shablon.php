a71889a6659c46c68cd47e6ac521bd30da4d5ddb
AtilaRajkov <atilar777@gmail.com>
2019-02-21 18:08:33 +0100

    mislim da sam uspeo da isecem

---
+++ public/index_shablon.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once '../views/headView.php';
+require_once '../views/headerView.php';
+
+// Savio sam samo kao PLACEHOLDER da vidim da lsa msve dobro povezao
+// kasnije cemo da obrisemo ovo
+require_once '../views/contentViewPLACEHOLDER.php';
+
+include_once '../views/footerView.php';
